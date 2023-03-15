9a6de4470267b23207a6b8a3a1e819693d67f904
桃山さわ <momoyama.sawa@gmail.com>
2023-03-15 16:52:49 +0800

    feat: 初始化
    一个初始场景和第一人称控制脚本

---
+++ Assets/Scripts/Player_LookControl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_LookControl : MonoBehaviour
+{
+
+    // 鼠标灵敏度
+    public float _mouseSensitivity = 200.0f;
+    float _kcameraX = 0.0f;
+    // 视角上下移动需要设定最大最小角度
+    const float _kcameraXMax = 60f;
+    const float _kcameraXMin = -60f;
+
+    // 玩家
+    public Transform _playerT = null;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 鼠标锁定
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible= false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 获取鼠标偏移
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * _mouseSensitivity;
+
+        // 视角上下移动
+        _kcameraX -= mouseY;
+        // 在最大值最小值之间取值
+        _kcameraX = Mathf.Clamp(_kcameraX, _kcameraXMin, _kcameraXMax);
+        // 摄像机旋转
+        transform.localRotation = Quaternion.Euler(_kcameraX, 0, 0);
+
+
+        // 视角的左右移动，一般是360度，不需要额外的存储变量，摄像机是人物的子物体，直接转人即可
+        if (_playerT != null)
+        {
+            _playerT.Rotate(_playerT.up, mouseX);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player_MoveControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_MoveControl : MonoBehaviour
+{
+    // 移动的速度
+    public float _walkSpeed;
+    // 重力
+    public float _gravity;
+    // 跳跃高度
+    public float _jumpHeight;
+    // 角色控制器
+    CharacterController _player = null;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 获取水平垂直轴的偏移
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        // 确定前进方向
+        Vector3 moveDir = transform.right * h + transform.forward * v;
+        // 重力模拟
+        Vector3 downMove = -_player.transform.up * _gravity * Time.deltaTime;
+        //// 跳跃
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    moveDir += transform.up * _jumpHeight;
+        //}
+        // 移动
+        _player.Move(downMove + moveDir * _walkSpeed * Time.deltaTime);
+
+    }
+}
